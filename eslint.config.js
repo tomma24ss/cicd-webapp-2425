9103e481c8a31bef93267c2aefbad80def53d314
tomma24ss <tomma.vlaemynck@gmail.com>
2024-10-20 14:14:50 +0200

    Migrate ESLint configuration to eslint.config.js format

---
+++ eslint.config.js
@@ -0,0 +1,29 @@
+export default [
+  {
+    ignores: ['node_modules/**'],
+  },
+  {
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'module',
+    },
+    env: {
+      es6: true,
+      es2021: true,
+      node: true,
+      mocha: true,
+    },
+    rules: {
+      indent: ['error', 2, { SwitchCase: 1 }],
+      'linebreak-style': ['error', 'unix'],
+      quotes: ['error', 'single'],
+      semi: ['error', 'always'],
+      'comma-dangle': ['error', 'always-multiline'],
+      'no-tabs': ['error'],
+      'max-len': ['error', { code: 120, tabWidth: 2 }],
+      'arrow-parens': ['error', 'always'],
+      'brace-style': ['error', '1tbs', { allowSingleLine: false }],
+      'no-inner-declarations': 'off',
+    },
+  },
+];
